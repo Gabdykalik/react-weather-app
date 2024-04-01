f00e6e6c7ecef8216901148289630a23fb061ddd
Gabdykalik <adletgabdykalik@gmail.com>
2024-04-01 17:17:55 +0500

    first push

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bb352b37c7msh4464643a4644ea4p13ee41jsn1643fca2c0e4",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "33f2d41e806502be7aca936dbc211483";
